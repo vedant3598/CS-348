539d4c49372acfc39ffd64ccbf787508b770466f
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2023-01-24 00:07:46 -0500

    Creating helloworld.py for milestone 0

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import pyodbc 
+
+print("Hello World")
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=server_name;'
+                      'Database=database_name;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM table_name')
+
+for i in cursor:
+    print(i)
