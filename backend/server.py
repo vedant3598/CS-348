6d2062c3c73605880b96ae1ae18dcd88ed566d44
jaybhagat <42147408+jaybhagat@users.noreply.github.com>
2023-03-05 21:47:16 -0500

    added new endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,19 @@
-from flask import Flask
-from backend_sql import get_medals_for_country
+from flask import Flask, request
+from backend_sql import get_medals_for_country, get_athletes_by_country
 
 # Set up flask app
 app = Flask(__name__)
 
 
-# Api endpoint to get all the countries and their total medal counts
-@app.route("/medals", methods=["GET"])
+# Get all the countries and their total medal counts
+@app.route("/country-medals", methods=["GET"])
 def get_all_medals():
     return get_medals_for_country()
+
+# Get all the athletes by country
+@app.route("/country-athletes", methods=["GET"])
+def get_athlete_medals():
+    return get_athletes_by_country(request.args.get('country'))
 
 
 app.run(host='localhost', port=5000)
